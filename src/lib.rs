53328358655463ab6286373314ad15e4513ff172
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-12 22:37:36 -0400

    v0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 extern crate libc;
 
-extern {
+extern "C" {
     pub fn gnostr_get_relays(input: libc::c_int) -> libc::c_int;
 }
-extern {
+extern "C" {
     pub fn get_list() -> libc::c_char;
 }
 
 pub fn get_relays(input: i32) {
     let _output = unsafe { gnostr_get_relays(input) };
-	let _result = input*2;
+    let _result = input * 2;
     //println!("{} * 2 = {}", input, _result);
 }
--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,24 @@ extern crate gnostr_get_relays;
 
 fn do_work(inp: &str, _out: Option<String>) {
     //println!("{}", inp);
-    let mut input: i32 = inp
-        .trim()
-        .parse()
-        .expect("Wanted a number");
+    let mut input: i32 = inp.trim().parse().expect("Wanted a number");
 
     if input < 100 {
-      input = 100;
+        input = 100;
     }
 
     //let _output = unsafe { gnostr_get_relays::gnostr_get_relays(input) };
     let _output = /*unsafe {*/ gnostr_get_relays::get_relays(input)/* }*/;
-	//println!("output....\n{:?}", _output);
-
+    //println!("output....\n{:?}", _output);
 
     let _get_list = unsafe { gnostr_get_relays::get_list() };
-	println!("{:?}", _get_list);
-
+    println!("{:?}", _get_list);
 }
 
 pub fn print_usage(program: &str, opts: &Options) {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
 }
-
 
 //pub fn print_usage(program: &str, opts: &Options) {
 //    let brief = format!("Usage: {} [options]", program);
@@ -48,7 +42,6 @@ pub fn print_input(_inp: &str, out: Option<String>) {
 }
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     //REF: https://docs.rs/getopts/latest/getopts/struct.Options.html
@@ -77,13 +70,12 @@ fn main() {
             process::exit(0);
         }
 
+        let output = matches.opt_str("o");
+        //leave input as &Option<String>
+        let input = matches.opt_str("i");
+        //defed &str
+        let value = input.as_deref().unwrap_or("100");
 
-    let output = matches.opt_str("o");
-		//leave input as &Option<String>
-    let input = matches.opt_str("i");
-		//defed &str
-    let value = input.as_deref().unwrap_or("100");
-
-    do_work(&value, output);
+        do_work(&value, output);
     }
 }
